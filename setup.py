0b36f800b428bb79f356113f18cd628637ccbcbb
statsrcool <collin@collinparan.com>
2023-05-31 17:46:17 -0600

    updated short description

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='strategais',
-    version='0.1', 
+    version='0.1.1', 
+    description='A Python library for deploying large language models (LLMs) in local environments.',
     long_description=long_description,
     long_description_content_type="text/markdown", 
     packages=find_packages(),
